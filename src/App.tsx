a91be56037e673923782b94febaa294a26207a96
James Wang <jmaeswnag@gmail.com>
2025-03-13 22:12:03 -0400

    Implemented skeleton of API code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import MemoryCards from "./components/MemoryCards";
+import photoData from "./utils";
 
-function App() {
-  const [count, setCount] = useState(0)
+const PHOTO_ENDPOINT = "https://jsonplaceholder.typicode.com/photos/1";
+
+const NUM_PHOTOS = 12;
+
+const App = () => {
+  const [score, setScore] = useState<number>(0);
+  const [bestScore, setBestScore] = useState<number>(0);
+  const [photos, setPhotos] = useState<photoData[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      try {
+        setIsLoading(true);
+
+        const fetchPhoto = async (url: string) => {
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
+          }
+          return response.json();
+        };
+
+        const photo_urls = [];
+        for (let i = 0; i < NUM_PHOTOS; i++) {
+          photo_urls.push(PHOTO_ENDPOINT);
+        }
+        const photos = await Promise.all(
+          photo_urls.map((url) => fetchPhoto(url))
+        );
+        const photoInfos: photoData[] = photos.map((photoJson) => {
+          return { src: photoJson.url, description: photoJson.title };
+        });
+        setPhotos(photoInfos);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        if (error instanceof Error) {
+          setError(error.message);
+          setIsLoading(false);
+        }
+      }
+    };
+    fetchPhotos();
+
+    return () => {
+      setError(null);
+      setIsLoading(true);
+    };
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>Random Photos Memory Game</h1>
+      <p>
+        Get points by clicking on an image but don't click on any more than
+        once!
       </p>
+      <div>
+        <p>Score: {score}</p>
+        <p>Best score: {bestScore}</p>
+      </div>
+      <MemoryCards
+        photos={photos}
+        score={score}
+        setScore={setScore}
+        bestScore={bestScore}
+        setBestScore={setBestScore}
+      />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
